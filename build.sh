6869eb0f2ead5c0125681d627406ad8bf68bf8c8
bus1hero <petru.cervac@gmail.com>
2022-11-12 17:56:36 +0200

    build: update the install url

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ BUILD_PROJECT_FILE="$SCRIPT_DIR/build/_build.csproj"
 TEMP_DIRECTORY="$SCRIPT_DIR//.nuke/temp"
 
 DOTNET_GLOBAL_FILE="$SCRIPT_DIR//global.json"
-DOTNET_INSTALL_URL="https://dot.net/v1/dotnet-install.sh"
+DOTNET_INSTALL_URL="https://dotnet.microsoft.com/download/dotnet/scripts/v1/dotnet-install.sh"
 DOTNET_CHANNEL="Current"
 
 export DOTNET_CLI_TELEMETRY_OPTOUT=1
