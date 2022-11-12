00d2cae02d64400463b78764db0f27a31d270382
bus1hero <petru.cervac@gmail.com>
2022-11-12 23:40:34 +0200

    build: add steps to create output for github actions

--- build/Build.cs
+++ build/Build.cs
@@ -4,6 +4,11 @@ using Nuke.Common.Tools.DotNet;
 using Nuke.Common.IO;
 using System;
 using System.IO;
+using Nuke.Common.Tooling;
+using System.Collections.Generic;
+using static GitHubActionTasks;
+using System.Threading.Tasks;
+using System.Linq;
 
 class Build : NukeBuild
 {
@@ -17,11 +22,9 @@ class Build : NukeBuild
 	Target SetOutputs => _ => _
 		.Executes(async () =>
 		{
-			var filename = Environment.GetEnvironmentVariable("GITHUB_OUTPUT");
-			using var file = File.OpenWrite(filename);
-			using var filewriter = new StreamWriter(file);
-			await filewriter.WriteLineAsync("FAV_NUMBER=5");
-			await filewriter.WriteLineAsync("FAV_COLOR=blue");
+			await SetStepOutput(_ => _
+				.AddOutput("FAV_NUMBER", "5")
+				.AddOutput("FAV_COLOR", "blue"));
 		});
 
 	Target Publish => _ => _
@@ -36,4 +39,53 @@ class Build : NukeBuild
 		{
 
 		});
+}
+
+public class GitHubActionSetting
+{
+	public Dictionary<string, string> Outputs { get; }
+		= new Dictionary<string, string>();
+}
+
+public static class GitHubActionTasks
+{
+	public static async Task SetStepOutput(
+		Configure<GitHubActionSetting> configurator)
+	{
+		if (configurator is null)
+		{
+			throw new ArgumentNullException(nameof(configurator));
+		}
+
+		var settings = configurator(new GitHubActionSetting());
+		await WriteOutputs(settings.Outputs);
+	}
+
+	private static async Task WriteOutputs(IDictionary<string, string> outputs)
+	{
+		if (outputs.Count == 0)
+		{
+			return;
+		}
+
+		var result = string.Join('\n', outputs.Select(pair => $"{pair.Key}={pair.Value}"));
+		var githubOutputFilename = Environment.GetEnvironmentVariable("GITHUB_OUTPUT");
+		using var file = File.OpenWrite(githubOutputFilename);
+		using var filewriter = new StreamWriter(file);
+		await filewriter.WriteLineAsync(result);
+	}
+
+	public static async Task SetStepOutput() => await SetStepOutput(_ => _);
+}
+
+public static class GitHubActionSettingExtensions
+{
+	public static GitHubActionSetting AddOutput(
+		this GitHubActionSetting settings,
+		string name,
+		string value)
+	{
+		settings.Outputs[name] = value;
+		return settings;
+	}
 }
