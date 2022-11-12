b1f9d50033ef372d87d2dff8752d5207dfd70472
bus1hero <petru.cervac@gmail.com>
2022-11-12 13:53:30 +0200

    feat: add a new project

--- build/Build.cs
+++ build/Build.cs
@@ -1,8 +1,23 @@
 using Nuke.Common;
+using static Nuke.Common.Tools.DotNet.DotNetTasks;
+using Nuke.Common.Tools.DotNet;
+using Nuke.Common.IO;
 
 class Build : NukeBuild
 {
-	public static int Main() => Execute<Build>(x => x.Release);
+	public static int Main() => Execute<Build>(x => x.Publish);
+
+	private AbsolutePath ProjectPath => RootDirectory / "console";
+
+	[Parameter]
+	private AbsolutePath PublishFolder => RootDirectory / "publish";
+
+	Target Publish => _ => _
+		.Executes(() => DotNetPublish(_ => _
+			.SetProject(ProjectPath)
+			.SetPublishProfile(Configuration.Release)
+			.SetOutput(PublishFolder)));
+
 
 	Target Release => _ => _
 		.Executes(() =>
